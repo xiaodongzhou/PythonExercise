0d04c67da9a0e2fe421e34ce33301e8d709d4f4b
Xiaodong Zhou <xdzhou@gmail.com>
2017-01-20 22:57:22 +0800

    009.py is updated for more usage on time

--- 009.py
+++ 009.py
@@ -1,7 +1,16 @@
 import time
 
 one_line = raw_input("please input a line.  You can see it after 3 seconds. ")
+
 print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+start_time = time.ctime(time.time());
+print start_time
+print time.time()
+
 time.sleep(3)
+
+print time.ctime(time.time())
 print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
 print one_line
